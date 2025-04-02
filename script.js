aa02418af348bdcbddf6558fddb9c72bc5b13b77
TANIA MENEGUSSO <110191073+menegusso21@users.noreply.github.com>
2025-04-02 00:09:54 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
@@ -11,18 +11,25 @@ document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    const alternaContraste = document.getElementById('alterna-contraste')
+
     let tamanhoAtualFonte = 1;
 
-    aumentaFonteBotao.addEventListener('click', function(){
+    aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
-    });
+    })
 
-    diminuiFonteBotao.addEventListener('click', function(){
+    diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
-    });
+    })
 
-});
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+
+
+})
