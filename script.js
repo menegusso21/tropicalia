636013c7bab189d775b49794ab77f40e8085fcd3
TANIA MENEGUSSO <110191073+menegusso21@users.noreply.github.com>
2025-04-06 17:44:08 -0300

    .

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-    const alternaContraste = document.getElementById('alterna-contraste')
-
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function () {
@@ -26,10 +24,4 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
-
-    alternaContraste.addEventListener('click', function () {
-        document.body.classList.toggle('alto-contraste')
-    })
-
-
 })
